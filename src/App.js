728caee9434f9e61b0922173093d1d9810db5764
amarnath <amarnathmandal097@gmail.com>
2023-05-29 23:41:28 +0530

    project done all major works are completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import Login from './views/Auth/Login';
+import Register from './views/Auth/Register';
+import Home from './views/Home/Home';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Messages from './views/Messages/Messages';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  const user = useSelector(state => state.user.currentUser);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={user ? <Navigate to='/messages' /> : <Home />} />
+          <Route path='/register/:identifier' element={!user ? <Register /> : <Navigate to='/messages' />} />
+          <Route path='/login' element={!user ? <Login /> : <Navigate to='/messages' />} />
+          <Route path='/messages' element={!user ? <Navigate to='/' /> : <Messages />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
